c1d4d4ec672e6b97e47f5f20cab0d2d968b4d4a3
Natthawut Hemathulin <natthawut.nux@gmail.com>
2015-08-07 22:02:10 +0700

    Simple-user-interface - Updated project.

--- simple-user-interface/app/src/main/java/me/cafecode/simpleuserinterface/MessageActivity.java
+++ simple-user-interface/app/src/main/java/me/cafecode/simpleuserinterface/MessageActivity.java
@@ -1,35 +1,22 @@
 package me.cafecode.simpleuserinterface;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button sendMessageButton = (Button) findViewById(R.id.button_send);
-        sendMessageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_message);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_message, menu);
         return true;
     }
 
